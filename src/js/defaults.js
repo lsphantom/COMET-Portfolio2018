4569ba68b5f8c0935d71962435c5fae134bed8cf
Gary <gp@limestripes.us>
2017-12-29 15:46:30 -0700

    add some scrollmagic

--- src/js/defaults.js
+++ src/js/defaults.js
@@ -1,5 +1,26 @@
 //COMET 2018 PORTFOLIO
 $(document).ready(function(){
+	//Init scrollmagic
+	var controller = new ScrollMagic.Controller();
+	
+	//index page scene
+	var indexScene = new ScrollMagic.Scene({
+		triggerElement: '.header-heading',
+		reverse: false
+	})
+	.setClassToggle('.intro-text', 'fade-in')
+	.addTo(controller);
+	
+
+
+
+	//our work scene
+	var workScene = new ScrollMagic.Scene({
+		triggerElement: '.portfolio-item-card',
+		reverse: false
+	})
+	.setClassToggle('.portfolio-item-card', 'fade-in')
+	.addTo(controller);
 
 
 });
